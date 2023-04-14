f53632a8660c07fb8970f8185db4667afabfc137
JackOliCode <jack.v.oliver@gmail.com>
2023-04-14 15:07:48 +0100

    added getCalendarEvents function and tested

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -82,3 +82,58 @@ module.exports.getAccessToken = async (event) => {
           };
         });
     };
+
+    module.exports.getCalendarEvents = event => {
+
+      const oAuth2Client = new google.auth.OAuth2(
+        client_id,
+        client_secret,
+        redirect_uris[0]
+      );  
+     // Decode authorization code extracted from the URL query
+      const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+      oAuth2Client.setCredentials({ access_token }); //destructioning access_token and referencing variable created above
+
+      return new Promise( (resolve, reject) => {
+
+        calendar.events.list(  //calendar variable declared at top of file
+          { // object argument
+            calendarId: calendar_id,
+            auth: oAuth2Client,
+            timeMin: new Date().toISOString(),
+            singleEvents: true,
+            orderBy: "startTime",
+          },
+          (error, response) => { // callback function argument
+            if (error) {
+              reject(error);
+            } else {
+              resolve(response);
+            }
+          }
+        );
+
+      })
+      .then( results => { //this is the response from above. The data from calendar app
+        return {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify({ events: results.data.items })
+        };
+      })
+      .catch((err) => {
+        // Handle error
+        console.error(err);
+        return {
+          statusCode: 500,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify(err),
+        };
+      });
+
+
+    }
