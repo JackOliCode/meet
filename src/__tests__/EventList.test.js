d49843b11d04da238eee81ff76fd7fc92998a48b
JackOliCode <jack.v.oliver@gmail.com>
2023-04-15 09:57:21 +0100

    Update EventList.test.js

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import EventList from '../EventList';
 
-describe('<EventList /> component', () => {
+describe('<EventList /> component', () => { // scope only check EventList component
   test('render correct number of events', () => {
-    const EventListWrapper = shallow(<EventList events={[{}, {}, {}, {}]} />);
+    const EventListWrapper = shallow(<EventList events={[{}, {}, {}, {}]} />); //four empty objects 'Mock' data
     expect(EventListWrapper.find(Event)).toHaveLength(4);
   });
 });
