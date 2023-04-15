df3966fc760da8cbb69a42399b1a488918bddb09
JackOliCode <jack.v.oliver@gmail.com>
2023-04-15 10:14:22 +0100

    added key to events and updated test

--- src/EventList.js
+++ src/EventList.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-
+import Event from './Event';
 
 class EventList extends Component {
     render() {
+        const { events } = this.props;
         return (
             <ul className='EventList'>
-
+                {events.map (event => //maps all events and returns each as a li
+                    <li key={event.id}>
+                        <Event event={event} />
+                    </li>
+                )}
             </ul>
         );
     }
--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import EventList from '../EventList';
+import Event from '../Event';
+
 
 describe('<EventList /> component', () => { // scope only check EventList component
   test('render correct number of events', () => {
-    const EventListWrapper = shallow(<EventList events={[{}, {}, {}, {}]} />); //four empty objects 'Mock' data
+    const EventListWrapper = shallow(<EventList events={[{ id: 1}, { id: 2 }, { id: 3 }, { id: 4 }]} />); //four empty objects 'Mock' data
     expect(EventListWrapper.find(Event)).toHaveLength(4);
   });
 });
