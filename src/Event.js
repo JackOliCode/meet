56278ff2d19ffef0311b084e044399ea836898c0
JackOliCode <jack.v.oliver@gmail.com>
2023-04-17 20:00:48 +0100

    added elements to event.js so that test passes

--- src/Event.js
+++ src/Event.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 
 class Event extends Component {
   render() {
-    return <div></div>;
-  }
+    return (
+    <div className="event">
+        <div className="event_name"></div>;
+        <div className="event_time"></div>;
+        <div className="event_summary"></div>;
+        <div className="event_location"></div>;
+    </div>
+  );
+}
 }
 export default Event;
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -9,8 +9,9 @@ describe('<Event /> component', () => { // scope only check Event component
         const event = mockData[0]; //select first event in mockData
         const EventWrapper = shallow(<Event event={event} />); // pass the selected event to the Event component
         expect(EventWrapper.find('.event')).toHaveLength(1); // make sure the event has the correct CSS class
+        expect(EventWrapper.find('.event_name')).toHaveLength(1); // make sure the event has a name element
         expect(EventWrapper.find('.event_time')).toHaveLength(1); // make sure the event has a time element
         expect(EventWrapper.find('.event_summary')).toHaveLength(1); // make sure the event has a summary element
         expect(EventWrapper.find('.event_location')).toHaveLength(1); // make sure the event has a location element  
-});
+    });
 });
