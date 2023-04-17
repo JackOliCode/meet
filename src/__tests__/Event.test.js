f82513602e644b937ed9ca3be326f93ef9308a61
JackOliCode <jack.v.oliver@gmail.com>
2023-04-17 23:11:08 +0100

    Changed 3rd test as state was already true.
    Decided not to make another button - just more code.

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -21,12 +21,23 @@ describe('<Event /> component', () => { // scope only check Event component
         expect(EventWrapper.find('.event_location')).toHaveLength(1); // make sure the event has a location element
         expect(EventWrapper.find('.event_details')).toHaveLength(1); // make sure the "Details" button is present  
     });
-    test('renders extra info when "details" button clickeed', () => {
+    test('renders extra info when "details" button clicked', () => {
         expect(EventWrapper.state('showDetails')).toBe(false); // make sure showDetails state is initially false
         EventWrapper.find('.event_details').simulate('click'); // simulate a click on the "Details" button
         expect(EventWrapper.state('showDetails')).toBe(true); // make sure showDetails state is now true
         expect(EventWrapper.find('.aboutEvent')).toHaveLength(1); // make sure the "About Event" heading is present
         expect(EventWrapper.find('.event_link')).toHaveLength(1); // make sure the link to Google Calendar is present
         expect(EventWrapper.find('.event_description')).toHaveLength(1); // make sure the description element is present
+    });
+    test('hide extra info when "hide details" button clicked', () => {
+        expect(EventWrapper.state('showDetails')).toBe(true); 
+        expect(EventWrapper.find('.aboutEvent')).toHaveLength(1); 
+        expect(EventWrapper.find('.event_link')).toHaveLength(1); 
+        expect(EventWrapper.find('.event_description')).toHaveLength(1); 
+        EventWrapper.find('.event_details').simulate('click'); // re-sim another click to hide details (need to add another button)
+        expect(EventWrapper.state('showDetails')).toBe(false); // make sure showDetails state is now false
+        expect(EventWrapper.find('.aboutEvent')).toHaveLength(0); // make sure the "About Event" heading is not present
+        expect(EventWrapper.find('.event_link')).toHaveLength(0); // make sure the link to Google Calendar is not present
+        expect(EventWrapper.find('.event_description')).toHaveLength(0); // make sure the description element is not present
     });
 });
