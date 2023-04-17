90a3aa2efb958be3501249cb18c8b89127c003d2
JackOliCode <jack.v.oliver@gmail.com>
2023-04-18 00:14:06 +0100

    NOE test 2 fail

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -4,8 +4,20 @@ import NumberOfEvents from '../NumberOfEvents';
 import { Component } from 'react';
 
 describe('<NumberOfEvents /> component', () => {
-    test('render text input', () => {
+    test('render number input', () => {
         const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
         expect(NumberOfEventsWrapper.find('.numberInput')).toHaveLength(1);
     });
+    test('rendered number input is equal to 32', () => {
+        const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+        const query = NumberOfEventsWrapper.state('query');
+        expect(NumberOfEventsWrapper.find('.numberInput').prop('value')).toBe(query);
+    });
 });
+
+/* tests to make;
+There needs to be 32 events displayed 'render 32 events as default' - eventList
+
+number 32 needs to be displayed on NOE component - NOE
+
+*/
