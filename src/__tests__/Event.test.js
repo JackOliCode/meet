1922bb7e15c6f7c25a7506a68e5cafcbd041820a
JackOliCode <jack.v.oliver@gmail.com>
2023-04-17 20:26:35 +0100

    added 2nd test for feature 2 - fail test

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -12,6 +12,17 @@ describe('<Event /> component', () => { // scope only check Event component
         expect(EventWrapper.find('.event_name')).toHaveLength(1); // make sure the event has a name element
         expect(EventWrapper.find('.event_time')).toHaveLength(1); // make sure the event has a time element
         expect(EventWrapper.find('.event_summary')).toHaveLength(1); // make sure the event has a summary element
-        expect(EventWrapper.find('.event_location')).toHaveLength(1); // make sure the event has a location element  
+        expect(EventWrapper.find('.event_location')).toHaveLength(1); // make sure the event has a location element
+        expect(EventWrapper.find('.event_details')).toHaveLength(1); // make sure the "Details" button is present  
+    });
+    test('renders extra info when "details" button clickeed', () => {
+        const event = mockData[0];
+        const EventWrapper = shallow(<Event event={event} />);
+        expect(EventWrapper.state('showDetails')).toBe(false); // make sure showDetails state is initially false
+        EventWrapper.find('.event_details').simulate('click'); // simulate a click on the "Details" button
+        expect(EventWrapper.state('showDetails')).toBe(true); // make sure showDetails state is now true
+        expect(EventWrapper.find('h1')).toHaveLength(1); // make sure the "About Event" heading is present
+        expect(EventWrapper.find('a')).toHaveLength(1); // make sure the link to Google Calendar is present
+        expect(EventWrapper.find('.event_description')).toHaveLength(1); // make sure the description element is present
     });
 });
