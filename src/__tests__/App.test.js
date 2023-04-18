be3f23f7033d64ed1e94f682edee5d69f3559e16
JackOliCode <jack.v.oliver@gmail.com>
2023-04-18 23:59:01 +0100

    First INT test for feature 1 scenario 3 - fail

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
 import NumberOfEvents from '../NumberOfEvents';
 
+// ------- UNIT TESTS START HERE --------- //
 describe('<App /> component', () => { // new scope 
     let AppWrapper;
     beforeAll(() => {
@@ -24,5 +25,17 @@ describe('<App /> component', () => { // new scope
         expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
       });
 
+});
+
+// ------- INTEGRATION TESTS START HERE --------- //
+describe('<App /> integration', () => {
+  test('App passes "events" state as a prop to EventList', () => {
+    const AppWrapper = mount(<App />);
+    const AppEventsState = AppWrapper.state('events');
+    expect(AppEventsState).not.toEqual(undefined); //  test checks whether the state of events isn’t undefined. necessary otherwise comparison below could pass with events being equal
+    expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState); //compare the state of App’s events with EventList's events prop to ensure it’s been passed correctly
+    AppWrapper.unmount(); // “clean up” your DOM after each test using a function called unmount()
+
+  });
 
 });
