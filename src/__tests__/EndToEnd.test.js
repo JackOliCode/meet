09a19f40fcc21b7a18b92ce34f4869e353cfdab8
JackOliCode <jack.v.oliver@gmail.com>
2023-04-26 00:00:29 +0100

    Update EndToEnd.test.js

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -4,31 +4,34 @@ import puppeteer from "puppeteer"; // had to roll back to 18.1 as was getting er
 
 
 describe('show/hide an event details', () => {
+    let browser;
+    let page;
+    beforeAll(async () => {
+      jest.setTimeout(30000);
+      browser = await puppeteer.launch(); // launches browser via Puppeteer
+      page = await browser.newPage();
+      await page.goto('http://localhost:3000/'); //navigate to page
+      await page.waitForSelector('.event'); //waits for .event element to appear
+    });
+  
+    afterAll(() => {
+      browser.close(); //close browser
+    });
     
     test('An event element is collapsed by default', async () => {
-        const browser = await puppeteer.launch(); // launches browser via Puppeteer
-
-        const page = await browser.newPage();
-        await page.goto('http://localhost:3000/'); //navigate to pageenpm 
-    
-        await page.waitForSelector('.event'); //waits for .event element to appear
-
         const eventDetails = await page.$('.event .expanded_event'); //returns no result as element is collapsed
-    expect(eventDetails).toBeNull();
-    browser.close();
+        expect(eventDetails).toBeNull();
   });
 
   test('User can expand an event to see its details', async () => {
-    const browser = await puppeteer.launch();
-
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-
-    await page.waitForSelector('.event');
     await page.click('.event .event_details_btn');
-
     const eventDetails = await page.$('.event .expanded_event');
     expect(eventDetails).toBeDefined();
-    browser.close();
+  });
+
+  test('User can collapse an event to hide its details', async () => {
+    await page.click('.event .event_details_btn');
+    const eventDetails = await page.$('.event .expanded_event');
+    expect(eventDetails).toBeNull();
   });
 });
