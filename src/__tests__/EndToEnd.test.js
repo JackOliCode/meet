74cace254451f9149a56395114f464d3d0d16998
JackOliCode <jack.v.oliver@gmail.com>
2023-04-25 22:40:25 +0100

    Update EndToEnd.test.js

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,19 +1,34 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer"; // had to roll back to 18.1 as was getting error
 
 // ---------------- Before you can run your tests, you have to deploy your app using npm run start in the terminal. --------//
 
 
 describe('show/hide an event details', () => {
+    
     test('An event element is collapsed by default', async () => {
         const browser = await puppeteer.launch(); // launches browser via Puppeteer
 
         const page = await browser.newPage();
-        await page.goto('http://localhost:3000/'); //navigate to page
+        await page.goto('http://localhost:3000/'); //navigate to pageenpm 
     
         await page.waitForSelector('.event'); //waits for .event element to appear
 
         const eventDetails = await page.$('.event .expanded_event'); //returns no result as element is collapsed
     expect(eventDetails).toBeNull();
+    browser.close();
+  });
+
+  test('User can expand an event to see its details', async () => {
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+
+    await page.waitForSelector('.event');
+    await page.click('.event .event_details_btn');
+
+    const eventDetails = await page.$('.event .expanded_event');
+    expect(eventDetails).toBeDefined();
     browser.close();
   });
 });
