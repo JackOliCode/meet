da7a14a9dec2881c6778c1002820bd0e51d0dff3
JackOliCode <jack.v.oliver@gmail.com>
2023-05-10 12:13:26 +0100

    added warning alert to app.js, hopefully

--- src/App.js
+++ src/App.js
@@ -5,26 +5,47 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { extractLocations, getEvents } from './api';
 import './nprogress.css';
+import WarningAlert from './Alert';
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
-    numberOfEvents: 32
+    numberOfEvents: 32,
+    isOffline: !navigator.onLine, // Add a flag to track the online/offline status
   };
 
   componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
+        this.setState({
+          events,
+          locations: extractLocations(events),
+          isOffline: !navigator.onLine,
+        });
       }
     });
-  }
+  
 
-  componentWillUnmount(){
-    this.mounted = false;
-  }
+  // Add an event listener to track the online/offline status
+  window.addEventListener('offline', this.handleOfflineStatus);
+  window.addEventListener('online', this.handleOnlineStatus);
+}
+
+componentWillUnmount() {
+  this.mounted = false;
+  window.removeEventListener('offline', this.handleOfflineStatus);
+  window.removeEventListener('online', this.handleOnlineStatus);
+}
+
+handleOfflineStatus = () => {
+  this.setState({ isOffline: true });
+};
+
+handleOnlineStatus = () => {
+  this.setState({ isOffline: false });
+};
 
   updateEvents = (location, eventCount) => { // added eventCount 
     getEvents().then((events) => {
@@ -48,6 +69,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {this.state.isOffline && <WarningAlert />}
         <div className='search_inputs'>
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents} />
