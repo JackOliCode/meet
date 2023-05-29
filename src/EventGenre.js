6001f0d043c618d6eabd322ecf7532308a159cdb
JackOliCode <jack.v.oliver@gmail.com>
2023-05-29 17:50:46 +0100

    Edited pie graph to remove labels for 0% and to give colors to previously missing genre

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 
 const EventGenre = ({ events }) => {
     const [data, setData] = useState([]);
-    const colors = ['#42F2F7', '#F61067', '#248232', '#FFC800', '#8B2635'];
+    const colors = ['#42F2F7', '#F61067', '#248232', '#FFC800', '#8B2635', '#93FF96', '#E18AD4' ];
 
     useEffect(() => { setData(() => getData()); }, [events]);
     
@@ -32,7 +32,12 @@ return (
                 outerRadius={80}
                 fill='#8884d8'
                 dataKey="value"
-                label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}>
+                label={
+                    ({ name, percent, value }) => 
+                        value !== 0 ? `${name} ${(percent * 100).toFixed(0)}%` : null 
+                    }
+            
+            >
                 {
                 data.map((entry, index) => (
                     <Cell key={`cell-${index}`} fill={colors[index]} />
